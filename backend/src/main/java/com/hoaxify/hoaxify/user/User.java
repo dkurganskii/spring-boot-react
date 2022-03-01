6846a3894a38a632e1d5f4546108a9c54a877ba4
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-03-01 07:33:34 -0600

    set up project

---
+++ backend/src/main/java/com/hoaxify/hoaxify/user/User.java
@@ -0,0 +1,11 @@
+package com.hoaxify.hoaxify.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userName;
+    private String displayName;
+    private String password;
+
+}
