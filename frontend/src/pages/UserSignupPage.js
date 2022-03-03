b6404be3ac9f60d1d1db5597172aa14b9ab34ee4
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-03-03 07:02:47 -0600

    add tests for signup page

---
+++ frontend/src/pages/UserSignupPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export class UserSignupPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <div><input placeholder="Your display name" /></div>
+                <div><input placeholder="Your username" /></div>
+                <div><input type="password" placeholder="Your password" /></div>
+                <div><input type="password" placeholder="Repeat your password" /></div>
+                <div><button>Sign Up</button></div>
+            </div>
+        )
+    }
+}
+
+export default UserSignupPage;
