08258c26acfc4d3e39cd0c2287b61b735986664c
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-03-04 06:54:22 -0600

    handling input change

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,14 +1,54 @@
 import React from 'react'
 
 export class UserSignupPage extends React.Component {
+
+    state = {
+        displayName: '',
+        username: '',
+        password: '',
+        passwordRepeat: ''
+    }
+
+    onChangeDisplayName = (event) => {
+        const value = event.target.value
+        this.setState({ displayName: value })
+    }
+
+    onChangeUsername = (event) => {
+        const value = event.target.value
+        this.setState({ username: value })
+    }
+
+    onChangePassword = (event) => {
+        const value = event.target.value
+        this.setState({ password: value })
+    }
+    onChangePasswordRepeat = (event) => {
+        const value = event.target.value
+        this.setState({ passwordRepeat: value })
+    }
+
     render() {
         return (
             <div>
                 <h1>Sign Up</h1>
-                <div><input placeholder="Your display name" /></div>
-                <div><input placeholder="Your username" /></div>
-                <div><input type="password" placeholder="Your password" /></div>
-                <div><input type="password" placeholder="Repeat your password" /></div>
+                <div><input
+                    placeholder="Your display name"
+                    value={this.state.displayName}
+                    onChange={this.onChangeDisplayName}
+                /></div>
+                <div><input placeholder="Your username"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                /></div>
+                <div><input type="password" placeholder="Your password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                /></div>
+                <div><input type="password" placeholder="Repeat your password"
+                    value={this.state.passwordRepeat}
+                    onChange={this.onChangePasswordRepeat}
+                /></div>
                 <div><button>Sign Up</button></div>
             </div>
         )
