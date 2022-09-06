def7db6e80658dfe7231b0b6784c1d0459725819
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-09-06 09:41:59 -0500

    finish topBar

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from '../assets/hoaxify-logo.png'
+import {Link} from 'react-router-dom'
 
 class TopBar extends React.Component{
     render(){
@@ -7,10 +8,21 @@ class TopBar extends React.Component{
             <div className="bg-white shadow-sm mb-2">
                 <div className="container">
             <nav className="navbar navbar-light navbar-expand">
-                <a href="/">
-                <img src={logo} width="60" alt="Hoaxify"/>
-                </a>
-              
+                <Link to="/" className="navbar-brand">
+                <img src={logo} width="60" alt="Hoaxify"/> Hoaxify
+                </Link>
+                <ul className="nav navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/signup" className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+            </ul>
             </nav>
             </div>
             </div>
