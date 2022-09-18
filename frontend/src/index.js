1f6f8414b890aea623c3830cd8efde2e5cb86c54
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2022-09-18 08:53:40 -0500

    dispatching action

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './containers/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import authReducer from './redux/authReducer';
+import logger from 'redux-logger';
 
 const loggedInState = {
   id: 1,
@@ -15,7 +16,7 @@ const loggedInState = {
   password: 'P4ssword',
   isLoggedIn: true
 };
-const store = createStore(authReducer, loggedInState);
+const store = createStore(authReducer, loggedInState, applyMiddleware(logger));
 
 
 ReactDOM.render(
